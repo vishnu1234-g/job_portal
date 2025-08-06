a2087fa5b4bd72a0d4367ac672935d94e62b2a67
vishnu1234-g <123488304+vishnu1234-g@users.noreply.github.com>
2025-08-06 23:41:53 +0530

    Update dbConnection.js

--- react-job-portal/backend/database/dbConnection.js
+++ react-job-portal/backend/database/dbConnection.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose"; //just mongoose import!
+import mongoose from "mongoose"; 
 import dotenv from "dotenv"
 dotenv.config()
 
-//Database connection here!
+
  const dbConnection  = ()=>{
     mongoose.connect(process.env.DB_URL,{
        dbName: "Job_Portal"
 
-    }).then(()=>{ //agar connect ho jaye toh!
+    }).then(()=>{ 
        console.log("MongoDB Connected Sucessfully !")
     }).catch((error)=>{
-        console.log(`Failed to connect ${error}`) //warna error de do console me!
+        console.log(`Failed to connect ${error}`) 
     })
     
 }
